6018305e7d797e15896cc958fe6b87a7126658ef
Child <97318563+Igudlx@users.noreply.github.com>
2025-12-24 21:27:52 -0500

    Update register.js

--- api/register.js
+++ api/register.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import { sql } from "@vercel/postgres";
+import { sql } from "@vercel/postgres";asasd
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
@@ -36,3 +36,4 @@ export default async function handler(req, res) {
     return res.status(500).send("Server error");
   }
 }
+
